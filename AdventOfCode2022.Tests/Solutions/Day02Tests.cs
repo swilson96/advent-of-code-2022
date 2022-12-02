770d6d3df62d797413a4fbf9d8748a1e417f5592
Simon Wilson <simon.wilson@hudl.com>
2022-12-02 06:58:16 +0000

    day 2

--- AdventOfCode2022.Tests/Solutions/Day02Tests.cs
+++ AdventOfCode2022.Tests/Solutions/Day02Tests.cs
@@ -4,14 +4,16 @@ namespace AdventOfCode2022.Tests.Solutions;
 
 public class Day02Tests
 {
-    private const string ExampleInput = @"";
+    private const string ExampleInput = @"A Y
+B X
+C Z";
     
     [Fact]
     public void PartOneExample()
     {
         var result = new Day02().PartOne(ExampleInput);
         
-        Assert.Equal(1, result);
+        Assert.Equal(15, result);
     }
 
     [Fact]
@@ -19,6 +21,6 @@ public class Day02Tests
     {
         var result = new Day02().PartTwo(ExampleInput);
             
-        Assert.Equal(1, result);
-}
+        Assert.Equal(12, result);
+    }
 }
--- AdventOfCode2022/Solutions/Day02.cs
+++ AdventOfCode2022/Solutions/Day02.cs
@@ -1,14 +1,54 @@
+using System.Diagnostics;
+
 namespace AdventOfCode2022.Solutions;
 
 public class Day02 : IAdventSolution
 {
     public int PartOne(string input)
     {
-        return 0;
+        return input.Split(Environment.NewLine)
+            .Select(ScoreRound)
+            .Sum();
+    }
+
+    private int ScoreRound(string round)
+    {
+        return round switch
+        {
+            "A X" => 4, // rock rock
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7, // 6 + 1
+            "C Y" => 2, // 0 + 2
+            "C Z" => 6, // 3 + 3
+            _ => 0
+        };
     }
 
     public int PartTwo(string input)
     {
-        return 0;
+        return input.Split(Environment.NewLine)
+            .Select(ScoreRoundFromResult)
+            .Sum();;
+    }
+    
+    private int ScoreRoundFromResult(string round)
+    {
+        return round switch
+        {
+            "A X" => 3, // rock lose: play scissors: 0 + 3
+            "A Y" => 4, // 3 + 1
+            "A Z" => 8, // 6 + 2
+            "B X" => 1, // 0 + 1
+            "B Y" => 5, // 3 + 2
+            "B Z" => 9, // 6 + 3
+            "C X" => 2, // 0 + 2
+            "C Y" => 6, // 3 + 3
+            "C Z" => 7, // 6 + 1
+            _ => 0
+        };
     }
 }
