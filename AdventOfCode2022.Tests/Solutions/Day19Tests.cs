ec9f71f5585e5fc6552fae5fd882e4112c335a33
Simon Wilson <simon.wilson@hudl.com>
2022-12-19 22:40:38 +0000

    day 19 part 1

--- AdventOfCode2022.Tests/Solutions/Day19Tests.cs
+++ AdventOfCode2022.Tests/Solutions/Day19Tests.cs
@@ -2,22 +2,23 @@ using AdventOfCode2022.Solutions;
 
 namespace AdventOfCode2022.Tests.Solutions;
 
-public class TemplateTests
+public class Day19Tests
 {
-    private const string ExampleInput = @"";
+    private const string ExampleInput = @"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
     
     [Fact]
     public void PartOneExample()
     {
-        var result = new Template().PartOne(ExampleInput);
+        var result = new Day19().PartOne(ExampleInput);
         
-        Assert.Equal(0, result);
+        Assert.Equal(33, result);
     }
 
     [Fact]
     public void PartTwoExample()
     {
-        var result = new Template().PartTwo(ExampleInput);
+        var result = new Day19().PartTwo(ExampleInput);
         
         Assert.Equal(0, result);
     }
--- AdventOfCode2022/Solutions/Day19.cs
+++ AdventOfCode2022/Solutions/Day19.cs
@@ -1,8 +1,122 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2022.Solutions;
 
-public class Template : IAdventSolution
+public class Day19 : IAdventSolution
 {
-    public object PartOne(string input) => 0;
+    public object PartOne(string input) => input.Split(Environment.NewLine)
+        .Select(Blueprint.Parse)
+        .Select(ScoreBlueprint)
+        .Select((s, i) => s * (i + 1))
+        .Sum();
 
+    private int ScoreBlueprint(Blueprint blueprint)
+    {
+        var robots = Enum.GetValues<Material>().ToDictionary(v => v, _ => 0);
+        robots[Material.Ore] = 1;
+        var materials = Enum.GetValues<Material>().ToDictionary(v => v, _ => 0);
+
+        return Math.Max(ScoreBlueprint(blueprint, robots, materials, Material.Ore, 24),
+            ScoreBlueprint(blueprint, robots, materials, Material.Clay, 24));
+    }
+
+    private int ScoreBlueprint(Blueprint blueprint, Dictionary<Material, int> robots, Dictionary<Material, int> materials, Material nextToBuild, int timeLeft)
+    {
+        var geodesThisMinute = robots.ContainsKey(Material.Geode) ? robots[Material.Geode] : 0;
+        
+        if (timeLeft == 1)
+        {
+            return geodesThisMinute;
+        }
+        
+        var nextMaterials = materials.ToDictionary(e => e.Key, e => e.Value + robots[e.Key]);
+        if (blueprint.CanBuildWith(nextToBuild, materials))
+        {
+            ++robots[nextToBuild];
+            nextMaterials = blueprint.SubtractCost(nextToBuild, nextMaterials);
+            var score = geodesThisMinute + blueprint.ViableBuilds(robots).Select(r => ScoreBlueprint(blueprint, robots, nextMaterials, r, timeLeft - 1)).Max();
+            --robots[nextToBuild];
+            return score;
+        }
+        
+        return geodesThisMinute + ScoreBlueprint(blueprint, robots, nextMaterials, nextToBuild, timeLeft - 1);
+    }
+    
     public object PartTwo(string input) => 0;
+
+    private enum Material
+    {
+        Ore, Clay, Obsidian, Geode
+    }
+    
+    private class Blueprint
+    {
+        private static readonly Regex InputRegex =
+            new(
+                @"Blueprint \d+: Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\."); 
+        
+        // material to build => material to spend => amount
+        private readonly Dictionary<Material, Dictionary<Material, int>> _costs = new();
+
+        private readonly int _maxOreCost;
+
+        private Blueprint(int oreCost, int clayCost, int obsCostOre, int obsCostClay, int geoCostOre, int geoCostObs)
+        {
+            _costs[Material.Ore] = new Dictionary<Material, int> { { Material.Ore, oreCost } };
+            _costs[Material.Clay] = new Dictionary<Material, int> { { Material.Ore, clayCost } };
+            _costs[Material.Obsidian] = new Dictionary<Material, int> { { Material.Ore, obsCostOre }, { Material.Clay, obsCostClay } };
+            _costs[Material.Geode] = new Dictionary<Material, int> { { Material.Ore, geoCostOre }, { Material.Obsidian, geoCostObs } };
+
+            _maxOreCost = new List<int> { oreCost, clayCost, obsCostOre, geoCostOre }.Max();
+        }
+
+        private int Cost(Material toBuild, Material toSpend) => _costs[toBuild][toSpend];
+
+        public bool CanBuildWith(Material toBuild, Dictionary<Material, int> materials)
+        {
+            var costs = _costs[toBuild];
+            return costs.All(e => materials[e.Key] >= e.Value);
+        }
+        
+        public Dictionary<Material, int> SubtractCost(Material toBuild, Dictionary<Material, int> materials)
+        {
+            var costs = _costs[toBuild];
+            return materials.ToDictionary(e => e.Key, e => e.Value - (costs.ContainsKey(e.Key) ? costs[e.Key] : 0));
+        }
+        
+        public IEnumerable<Material> ViableBuilds(Dictionary<Material, int> robots)
+        {
+            if (robots[Material.Ore] < _maxOreCost)
+            {
+                yield return Material.Ore;
+            }
+
+            if (robots[Material.Clay] < _costs[Material.Obsidian][Material.Clay])
+            {
+                yield return Material.Clay;
+            }
+
+            if (robots[Material.Clay] > 0 && robots[Material.Obsidian] < _costs[Material.Geode][Material.Obsidian])
+            {
+                yield return Material.Obsidian;
+            }
+
+            if (robots[Material.Obsidian] > 0)
+            {
+                yield return Material.Geode;
+            }
+        }
+
+        public static Blueprint Parse(string inputLine)
+        {
+            var match = InputRegex.Match(inputLine);
+            return new Blueprint(
+                int.Parse(match.Groups[1].Value),
+                int.Parse(match.Groups[2].Value),
+                int.Parse(match.Groups[3].Value),
+                int.Parse(match.Groups[4].Value),
+                int.Parse(match.Groups[5].Value),
+                int.Parse(match.Groups[6].Value));
+        }
+    }
 }
